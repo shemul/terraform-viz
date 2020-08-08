81993d6c93f3c1f4aea2141c1ece76d4e45bffe0
Kief H. Shemul <kiefhossain@pathao.com>
2020-08-08 14:43:08 +0600

    delete not needed files

--- terraform/http.tf
+++
@@ -1,8 +0,0 @@
-data "http" "example" {
-  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
-
-  # Optional request headers
-  request_headers = {
-    Accept = "application/json"
-  }
-}
